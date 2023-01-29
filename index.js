2cc34f50c9421ef09273d0c9afe462863e2a48d4
Abhay Hingrajiya <abhayhingrajiya18@gmail.com>
2023-01-29 21:50:14 +0530

    add animation,create feedback

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+
+    /* ======================================
+            Start Display Area
+    ====================================== */
+    
     $('.collapse_plush').click(function(){
         $('.collapse_minus').css('display','block');
         $('.collapse_plush').css('display','none');
@@ -71,6 +76,69 @@ $(document).ready(function(){
         $('#racing_sub').css('display','none');
         $('.manufacturing_front').css('display','block');
     });
+
+    /* ======================================
+            End Display Area
+    ====================================== */
+
+    /* ======================================
+            Start Animation Area
+    ====================================== */
+
+    
+    $('.inquiry2').waypoint(function(){
+        $('.inquiry2').css({
+            animation: "inquiry2 3s forwards"
+        });
+    },{offset :"100%"
+    });
+    $('.left_15').waypoint(function(){
+        $('.left_15').css({
+            animation: "left_15 3s forwards"
+        });
+    },{offset :"100%"
+    });
+    $('.right_15').waypoint(function(){
+        $('.right_15').css({
+            animation: "right_15 3s forwards"
+        });
+    },{offset :"100%"
+    });
+    $('.left_5').waypoint(function(){
+        $('.left_5').css({
+            animation: "left_5 3s forwards"
+        });
+    },{offset :"100%"
+    });
+    $('.right_5').waypoint(function(){
+        $('.right_5').css({
+            animation: "right_5 3s forwards"
+        });
+    },{offset :"100%"
+    });
+    $('.bottom_6').waypoint(function(){
+        $('.bottom_6').css({
+            animation: "bottom_6 3s forwards"
+        });
+    },{offset :"120%"
+    });
+    $('.left_37').waypoint(function(){
+        $('.left_37').css({
+            animation: "left_37 3s forwards"
+        });
+    },{offset :"100%"
+    });
+    $('.left_0').waypoint(function(){
+        $('.left_0').css({
+            animation: "left_0 3s forwards"
+        });
+    },{offset :"100%"
+    });
+
+    /* ======================================
+            End Animation Area
+    ====================================== */
+    
     $.validator.addMethod("valueNotEquals", function(value, element, arg){
         return arg !== value;
     });
@@ -86,18 +154,28 @@ $(document).ready(function(){
                 valueNotEquals : 'none'
             }
         },
-        massages :{
-            name : 'This Field is required',
+        messages :{
+            name : 'Please Enter Your Name',
             email : {
-                required : 'This Field is required',
-                email : 'Enter Valid Email Address'
+                required : 'Please Enter Your Email Id',
+                email : 'Please Enter Valid Email Address'
             },
             selectInput : {
-                required : 'Select one option',
+                required : 'Please Select one option',
                 valueNotEquals : 'Select one option'
             }
         },
         submitHandler : function(){
+            // Email.send({
+			// 	Host: "",
+			// 	Port: ,
+			// 	Username: "",
+			// 	Password: "",
+			// 	To: "",
+			// 	From: "",
+			// 	Subject: "",
+			// 	Body: ""
+            // });
             alert('We have Your Details');
             alert('We will contact you very soon');
         }
